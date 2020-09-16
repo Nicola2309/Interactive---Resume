ae511d637ae0610b05a85eb004d5d3aa7ea70976
Nicola2309 <nicola.spigaglia@outlook.it>
2020-09-16 20:27:20 +0000

    Fixing Bugs, 'too many requests' msg implemented.1

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,17 +16,24 @@ function userInformationHTML(user) {
 }
 
 function repoInformationHTML(repos) {
-    if (repos.length === 0) {
+    if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
-    var listenItemsHTML = repos.map(function(repo) {
+    var listItemsHTML = repos.map(function(repo) {
         return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                    </li>`;
+                </li>`;
     });
 
-    return `<div class="clearfix repo-list"`
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -38,34 +45,35 @@ function fetchGitHubInformation(event) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+
     $("#gh-user-data").html(
         `<div id="loader">
-                <img src="assets/css/loader.gif" alt="loading..." />
-            </div>` );
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
 
-
-$.when(
-    $.getJSON(`https://api.github.com/users/${username}`),
-    $.getJSON(`https://api.github.com/users/${username}/repos`)
-).then(
-    function(firstResponse, secondResponse) {
-        var userData = firstResponse[0];
-        var repoData = secondResponse[0];
-        $("#gh-user-data").html(userInformationHTML(userData));
-        $("#gh-repo-data").html(userInformationHTML(repoData));
-    }, function(errorResponse) {
-        if (errorResponse.status === 404) {
-            $("#gh-user-data").html(
-            `<h2>No info found for user ${username}</h2>`);
-        } else if (errorResponse === 403) {
-            var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
-            $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleDateString()}</h4>`)
-        } else {
-            console.log(errorResponse);
-            $("#gh-user-data").html(
-                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-        }
-    });
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
 
 $(document).ready(fetchGitHubInformation);
